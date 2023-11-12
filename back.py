12b504fcf72518f712d58950e34370078c855d68
Caio Figueiredo <Contascontao@gmail.com>
2023-11-11 22:12:42 -0300

    I'm fucking exhausted. No funnies today

--- back.py
+++ back.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify, request
+from autofill import autofill
 from flask_cors import CORS
 import pandas as pd
 import numpy as np
@@ -11,38 +12,55 @@ meds = pd.DataFrame(pd.read_csv('static\sample_data_clean.csv', sep=','))
 
 
 def find_matches(df, search_string):
+    # Convert all words to lowercase for case-insensitive comparison
+    autofill_words = df['ds_micro_organismo'].str.lower().tolist()
+
+    # Register words with autofill (assuming autofill expects lowercase words)
+    autofill.register_words(autofill_words)
+    lower_search_string = search_string.lower()
+
+    # Perform the search
+    query = autofill.search(lower_search_string) if lower_search_string not in autofill_words else [
+        lower_search_string]
+
+    # Convert the DataFrame to lowercase once
+    lower_df = df.apply(lambda x: x.astype(str).str.lower())
+
     matches = []
+    for word in query:
+        # Finding the location
+        result = np.where(lower_df['ds_micro_organismo'] == word)
+        row_indices = result[0]
 
-    # Finding the location
-    result = np.where(df == search_string)
+        # Append matches
+        for row in row_indices:
+            matches.append((word, row))
 
-    # The result is a tuple with row indices and column indices
-    row_indices = result[0]
-    col_indices = result[1]
-
-    # Print the locations
-    for row, col in zip(row_indices, col_indices):
-        matches.append((row))
-        # print(f"String found at row {row}, column {col} (Column name: {df.columns[col]})")
     return matches
 
 
 @app.route('/message', methods=['POST'])
 def predict():
     data = request.get_json()
-    bacteria = data['bacteria']
-    if request.is_json:
-        print(bacteria)
+
+    if request.is_json and 'bacteria' in data:
+        bacteria = data['bacteria']
+
         results = []
         matches = find_matches(meds, bacteria)
+
         for i in range(len(matches)):
-            result = meds['ds_antibiotico_microorganismo'].iloc[matches[i]]
-            if pd.notna(result):
+            # Extracting antibiotic and micro_organism for each match
+            antibiotic = meds['ds_antibiotico_microorganismo'].iloc[matches[i][1]]
+            micro_organism = meds['ds_micro_organismo'].iloc[matches[i][1]]
+
+            # Check for non-null values and append to results
+            if pd.notna(antibiotic) and pd.notna(micro_organism):
+                result = (antibiotic, micro_organism)
                 results.append(result)
+
+        # Remove duplicates from results
         results = list(set(results))
-        if None in results:
-            results.remove(None)
-        print(results)
 
         return jsonify({'results': results}), 200
 
--- front.py
+++ front.py
@@ -1,8 +1,11 @@
 import streamlit as st
+from streamlit_modal import Modal
+import streamlit.components.v1 as html
 import requests
 
 
 def send_data_to_flask(data):
+    print(data)
     try:
         response = requests.post("http://localhost:5000/message", json=data)
         if response.status_code == 200:
@@ -17,9 +20,21 @@ def send_data_to_flask(data):
 
 def on_send_button_clicked():
     """Callback function for 'Send to Flask' button."""
-    response_data = send_data_to_flask({"bacteria": st.session_state.bacteria})
-    st.session_state.response_data = response_data
-    st.session_state.page = 'results'
+    youfailed = Modal(key="youfailed", title="You failed",
+                      max_width=500)
+    if st.session_state.bacteria == "" and st.session_state.bac_code == "":
+        with youfailed.container():
+
+            st.write("You failed")
+    else:
+        if st.session_state.bacteria:
+            response_data = send_data_to_flask(
+                {"bacteria": st.session_state.bacteria})
+        elif st.session_state.bac_code:
+            response_data = send_data_to_flask(
+                {"code": st.session_state.bac_code})
+        st.session_state.response_data = response_data
+        st.session_state.page = 'results'
 
 
 def on_go_back_button_clicked():
@@ -27,8 +42,10 @@ def on_go_back_button_clicked():
 
 
 def search_page():
-    st.title("Streamlit Frontend")
-    st.session_state.bacteria = st.text_input("Enter the name of the bacteria")
+    st.title("We doin this")
+
+    st.session_state.bacteria = st.text_input(
+        "Enter the name or code of the bacteria")
 
     st.button("Send to Flask", on_click=on_send_button_clicked)
 
@@ -37,25 +54,37 @@ def results_page():
     st.button("Go back to Search", on_click=on_go_back_button_clicked)
 
     response_data = st.session_state.get('response_data', {})
-    if 'results' in response_data:
+    if 'results' in response_data and response_data['results'] != []:
         st.write("Results from Flask:")
+
+        # Organize the results by disease
+        disease_to_antibiotics = {}
         for result in response_data['results']:
-            st.text(result)
+            antibiotic, micro_organism = result
+            if micro_organism not in disease_to_antibiotics:
+                disease_to_antibiotics[micro_organism] = [antibiotic]
+            else:
+                disease_to_antibiotics[micro_organism].append(antibiotic)
+
+        # Create tabs for each disease
+        tabs = st.tabs(list(disease_to_antibiotics.keys()))
+        for tab, (disease, antibiotics) in zip(tabs, disease_to_antibiotics.items()):
+            with tab:
+                st.write(f"Antibiotics for {disease}:")
+                for antibiotic in antibiotics:
+                    st.text(antibiotic)
     else:
         st.write("No results received or error in backend")
 
 
 def main():
-    st.image('https://www.streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png', width=200)
 
     if 'page' not in st.session_state:
         st.session_state['page'] = 'search'
 
     if st.session_state['page'] == 'search':
-        st.write("Displaying Search Page")
         search_page()
     elif st.session_state['page'] == 'results':
-        st.write("Displaying Results Page")
         results_page()
 
 
